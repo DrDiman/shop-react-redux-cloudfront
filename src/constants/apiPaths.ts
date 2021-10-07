027512484418804779a3d6a45e76fed8e4ba9ea8
Dzmitry Astraukh <dzmitry_astraukh@epam.com>
2021-10-08 02:14:18 +0300

    promote new path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,8 +3,8 @@ const API_PATHS = {
   product: 'https://a7o6p63xnd.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://a7o6p63xnd.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://k3ch3d467k.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://a7o6p63xnd.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://drdiman-cart-api-develop.eu-west-1.elasticbeanstalk.com/api',
+  bff: 'http://drdiman-bff-api-develop.eu-west-1.elasticbeanstalk.com',
+  cart: 'http://drdiman-bff-api-develop.eu-west-1.elasticbeanstalk.com',
 };
 
 export default API_PATHS;
